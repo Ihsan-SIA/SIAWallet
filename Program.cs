12bd5efe1d198a8d04c79faefe06bbc07a20a3f3
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-02-22 18:28:23 +0100

    Reoptimized the app for easy banking

--- Program.cs
+++ Program.cs
@@ -1,13 +1,4 @@
 ﻿using BankApp;
 
 Menu menu = new();
-// menu.MainMenu();
-Print(100);
-Print("Hello world");
-Print(5.5);
-
-static void Print<Generic>(Generic item)
-{
-    Console.WriteLine(item);
-}
-
+menu.MainMenu();
